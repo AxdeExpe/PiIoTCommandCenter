9476c375b19270c3d148f93780aa407e28d6d031
Kevin Pietzsch <s83799@htw-dresden.de>
2023-09-06 19:08:25 +0200

    add database brainstorming stuff

---
+++ src/network/server/microservices/database/database.cpp
@@ -0,0 +1,53 @@
+#include "database.h"
+
+Database::Database(){
+
+}
+
+bool Database::createDatabase(){
+
+}
+
+bool Database::deleteDatabase(){
+
+}
+
+
+
+
+bool Database::openDatabase(){
+
+}
+
+bool Database::closeDatabase(){
+
+}
+
+
+
+
+
+bool Database::createTable(){
+
+}
+
+bool Database::insertData(){
+
+}
+
+bool Database::updateData(){
+
+}
+
+bool Database::deleteData(){
+
+}
+
+bool Database::selectData(){
+
+}
+
+
+Database::~Database(){
+
+}
--- src/network/server/microservices/database/database.h
+++ src/network/server/microservices/database/database.h
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <sqlite3.h>
 
-class database{
+/*
+    Only the admin can use the database (Server-Raspi) 
+    -> means a direct insertion, update, delete or select is from outside (client -> server) not possible
+    -> the client can send a request to the server and the server can do the action but nothing more
+
+    Admin (Server-Raspi):
+        - Create Database
+        - Delete Database
+        - Create Table
+        - Insert Data
+        - Update Data
+        - Delete Data
+        - Select Data
+
+    User (Client):
+        - send requests to the server
+        - get responses from the server
+        -> client doesn't care directly about the databasestructure (tables, columns, rows, etc.)
+        -> client can request the structure of the database (tables, columns, rows, etc.)
+        -> client can send requests to the server but it is not possible to do a direct insertion, update, delete or select from the client side
+
+*/
+
+
+class Database{
     public:
-        database();
-        ~database();
+        Database();
+        ~Database();
+
+        bool createDatabase();
+        bool deleteDatabase();
 
         bool openDatabase();
         bool closeDatabase();
@@ -14,6 +41,8 @@ class database{
         bool updateData();
         bool deleteData();
         bool selectData();
+
+        //vector <string> getDatabasestructure(); // -> get the structure of the database (tables, columns, rows, etc.)
 
     private:
         sqlite3 *db;
